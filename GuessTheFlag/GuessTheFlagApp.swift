ba6c2ac68ad2f8303a13525fa5460544dfdae69c
Bruce Gilmour <bruce.gilmour@me.com>
2021-06-24 15:09:40 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Bruce Gilmour on 2021-06-24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
