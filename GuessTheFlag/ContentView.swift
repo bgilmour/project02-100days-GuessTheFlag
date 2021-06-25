c438e9a518e73d7a134e2942fefdd09840fdab2a
Bruce Gilmour <bruce.gilmour@me.com>
2021-06-25 12:57:42 +0100

    Completed solution for GuessTheFlag
    This change contains my completed solution for project 2 from the 100 Days Of SwiftUI tutorial created by Paul Hudson (@twostraws). It includes my interpretation of the three addition challenge steps.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0 ... 2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var currentScore = 0
+    @State private var correctCount = 0
+    @State private var wrongCount = 0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+
+                Spacer()
+
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        flagTapped(number)
+                    }) {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                            .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                            .overlay(RoundedRectangle(cornerRadius: 25, style: .continuous).stroke(Color.white, lineWidth: 1))
+                            .shadow(color: .black, radius: 3)
+                    }
+                }
+
+                Spacer()
+
+                Text("Correct: \(correctCount) Wrong: \(wrongCount)")
+                    .foregroundColor(.white)
+                    .font(.title3)
+            }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle),
+                  message: Text("Your score is \(currentScore)").font(.headline),
+                  dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+    }
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            correctCount += 1
+        } else {
+            scoreTitle = "That flag belongs to \(countries[number])"
+            wrongCount += 1
+        }
+        currentScore = correctCount - wrongCount
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0 ... 2)
     }
 }
 
